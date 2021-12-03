3d4db3b881125ea622ec1ec8d452184e5f0c2920
unknown <l.boukorras@gmail.com>
2021-12-03 19:39:02 +0100

    replacing requests with a json

--- Routes/cryptocurrencyListingsLatest.js
+++ Routes/cryptocurrencyListingsLatest.js
@@ -1,31 +1,34 @@
-const rp = require("request-promise");
+// const rp = require("request-promise");
 const express = require("express");
 const router = express.Router();
 
+const dataCryptocurrencyListingsLatest = require("../Data-CoinMarketCap/cryptocurrency-listings-latest.json");
+
 router.get("/cryptocurrencyListingsLatest", async (req, res) => {
   try {
-    const requestOptions = {
-      method: "GET",
-      uri: "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
-      qs: {
-        start: "1",
-        limit: "150",
-        convert: "USD",
-      },
-      headers: {
-        "X-CMC_PRO_API_KEY": process.env.API_KEY_CoinMarketCap,
-      },
-      json: true,
-      gzip: true,
-    };
+    // const requestOptions = {
+    //   method: "GET",
+    //   uri: "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
+    //   qs: {
+    //     start: "1",
+    //     limit: "1",
+    //     convert: "USD",
+    //   },
+    //   headers: {
+    //     "X-CMC_PRO_API_KEY": process.env.API_KEY_CoinMarketCap,
+    //   },
+    //   json: true,
+    //   gzip: true,
+    // };
 
-    rp(requestOptions)
-      .then((response) => {
-        res.json(response);
-      })
-      .catch((err) => {
-        console.log("API call error:", err.message);
-      });
+    // rp(requestOptions)
+    //   .then((response) => {
+    //     res.json(response);
+    //   })
+    //   .catch((err) => {
+    //     console.log("API call error:", err.message);
+    //   });
+    res.json(dataCryptocurrencyListingsLatest);
   } catch (error) {
     console.log(error.message);
     res.status(400).json({ error: error.message });
